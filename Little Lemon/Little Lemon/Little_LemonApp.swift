913067f50ee929337daea1e4c5d0a479744ef6f4
Claiton Puhl <claitonpuhl@hotmail.com>
2023-04-05 19:03:55 -0400

    create project

---
+++ Little Lemon/Little Lemon/Little_LemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_LemonApp.swift
+//  Little Lemon
+//
+//  Created by admin on 4/5/23.
+//
+
+import SwiftUI
+
+@main
+struct Little_LemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
